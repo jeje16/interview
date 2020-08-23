67e5175a8c22ea50139b29eeba6654bf48fcb9c1
jean <a.jcharmant@gmail.com>
2020-08-23 12:57:53 -0700

    updated api routes

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -23,12 +23,12 @@ class MainPage extends React.Component{
 
   displayMovies(){
     if(this.state.currentSearch==="popular"){
-        this.fetchIt("https://api.themoviedb.org/3/discover/movie?api_key=b59ee5e08e11d38120a14b2e670a381d&/discover/movie?sort_by=popularity.desc")        }
+        this.fetchIt("https://api.themoviedb.org/3/movie/popular?api_key=b59ee5e08e11d38120a14b2e670a381d")        }
     else if(this.state.currentSearch==="top"){
-        this.fetchIt("https://api.themoviedb.org/3/discover/movie?api_key=b59ee5e08e11d38120a14b2e670a381d&certification_country=US&sort_by=vote_average.desc")
+        this.fetchIt("https://api.themoviedb.org/3/movie/top_rated?api_key=b59ee5e08e11d38120a14b2e670a381d")
     }
     else if(this.state.currentSearch==="now"){
-        this.fetchIt("https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2020-08-15&primary_release_date.lte=2020-09-22&api_key=b59ee5e08e11d38120a14b2e670a381d");
+        this.fetchIt("https://api.themoviedb.org/3/movie/now_playing?api_key=b59ee5e08e11d38120a14b2e670a381d");
     }
     else{
         this.fetchIt('https://api.themoviedb.org/3/search/movie?api_key=b59ee5e08e11d38120a14b2e670a381d&query='+this.state.currentSearch)
